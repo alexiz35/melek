695f7621a255513718c6c31889a2bc180425f8ce
Aleksandr <aleksiz1977@gmail.com>
2022-08-20 22:39:54 +0300

    finished RunString

---
+++ components/LayoutMain.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Head from "next/head";
+import styles from "../styles/LayoutMain.module.css";
+import Marquee from "react-fast-marquee/dist";
+import Navbar2 from "./Navbar";
+import Container from "react-bootstrap/Container";
+
+export default function LayoutMain({children}) {
+    return (
+        <>
+            <header className={styles.header}>
+
+                <Marquee direction="right" speed={180} className={styles.runString}>
+                    177-й день війни росії проти України, наразі вже знищено 45000 російских загарбників
+                </Marquee>
+
+                <Navbar2/>
+
+            </header>
+
+
+            <main className={styles.container}>
+                {children}
+            </main>
+
+
+            {/*<Footer/>*/}
+        </>
+
+    )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,26 @@
 import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from "react";
+import Head from "next/head";
 
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}) {
+
+
+    return (
+        <>
+            <Head>
+                <title>Melek</title>
+                <meta name="keywords" content="text, keywords,rere"/>
+                <meta name="description" content="this is my first site on react-next"/>
+
+                <meta charSet="utf-8"/>
+            </Head>
+
+            <Component {...pageProps} />
+
+        </>
+    )
 }
 
 export default MyApp
