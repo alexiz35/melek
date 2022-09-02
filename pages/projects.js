96a3f08b9fb54090b5e1c358fc7e59d0b860d605
Aleksandr <aleksiz1977@gmail.com>
2022-09-02 21:25:12 +0300

    created Gallery in accordion v3

--- pages/projects.js
+++ pages/projects.js
@@ -10,7 +10,7 @@ import {useRouter} from "next/router";
 import Link from "next/link";
 
 
-export default function projects() {
+export default function Projects() {
 
     const router = useRouter()
 
